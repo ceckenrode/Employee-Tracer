c1f7119d8ff918543b253b7aa78b75b97116b211
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-10-22 22:05:57 -0400

    Added redux actions for handling login and account creation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
   constructor() {
     super();
     this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onButtonPress = this.onButtonPress.bind(this);
   }
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+            value={this.props.email}
             onChangeText={this.onEmailChange}
             label="Email"
             placeholder="email@gmail.com"
@@ -20,13 +23,15 @@ class LoginForm extends Component {
         </CardSection>
         <CardSection>
           <Input
+            value={this.props.password}
+            onChangeText={this.onPasswordChange}
             secureTextEntry
             label="Password"
             placeholder="password"
           />
         </CardSection>
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress}>
             Login
           </Button>
         </CardSection>
@@ -36,9 +41,21 @@ class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
   }
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+  onButtonPress() {
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password });
+  }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error } = auth;
 
-}
-export default connect(null, { emailChanged })(LoginForm);
+  return { email, password, error };
+};
+
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser,
+})(LoginForm);
